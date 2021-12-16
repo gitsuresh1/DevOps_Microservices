07ba62c71b19b9283b059eea236792b36456da13
Suresh B <send2suresh@gmail.com>
2021-12-17 00:34:11 +0530

    a

--- Lesson-1-Lambda-functions/wikipedia-query/HelloWiki/hello_world/app.py
+++ Lesson-1-Lambda-functions/wikipedia-query/HelloWiki/hello_world/app.py
@@ -1,33 +1,31 @@
 import json
+import wikipedia
+
+# prints when function loads
+print('Loading function')
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    ''' Wikipedia page summarizer.
+        :param event: a request with a wikipedia "entity" that has page information
+        :return: a response that contains the first sentence of a wikipedia page,
+            the response is JSON formatted.'''
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+    ## TO DO: Check that the request has some input body and save it
+    if 'body' in event:
+        body = None
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+    ## TO DO: Get the wikipedia "entity" from the body of the request
+    entity = None
+    res = wikipedia.summary(entity, sentences=1) # first sentence, result
 
-    context: object, required
-        Lambda Context runtime methods and attributes
+    # print statements
+    print(f"context: {context}, event: {event}")
+    print(f"Response from wikipedia API: {res}")
 
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-            {
-                "message": "hello world",
-            }
-        ),
+    ## TO DO: Format the response as JSON and return the result
+    response = {
+        ## your code here
     }
+
+    return response
